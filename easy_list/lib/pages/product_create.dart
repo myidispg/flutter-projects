50e46d10ad04c355312a8d6595fc96a1dcfab310
myidispg <myidispg@gmail.com>
2018-12-27 11:52:04 +0530

    Working on add product text fields

--- easy_list/lib/pages/product_create.dart
+++ easy_list/lib/pages/product_create.dart
@@ -1,14 +1,61 @@
 import 'package:flutter/material.dart';
 
-class ProductCreatePage extends StatelessWidget {
+class ProductCreatePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _ProductCreatePageState();
+  }
+}
+
+class _ProductCreatePageState extends State<ProductCreatePage> {
+  String titleValue = '';
+  String descriptionValue = '';
+  double priceValue = 0.0;
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: RaisedButton(onPressed: (){
-        showModalBottomSheet(context: context, builder: (BuildContext context){
-          return Center(child: Text('This is a modal'),);
-        });
-      }, child: Text('Save'),)
-    );
+    return Container(
+      margin: EdgeInsets.all(10.0),
+        child: ListView(
+      children: <Widget>[
+        TextField(
+          decoration: InputDecoration(
+              labelText: 'Product Title',
+              hintText: 'Enter product title here.'),
+          onChanged: (String value) {
+            setState(() {
+              titleValue = value;
+            });
+          },
+        ),
+        TextField(
+          maxLines: 4,
+          decoration: InputDecoration(
+              labelText: 'Product Description',
+              hintText: 'Enter product description here.'),
+          onChanged: (String value) {
+            setState(() {
+              descriptionValue = value;
+            });
+          },
+        ),
+        TextField(
+          keyboardType: TextInputType.number,
+          decoration: InputDecoration(
+              labelText: 'Product Price',
+              hintText: 'Enter product price here.'),
+          onChanged: (String value) {
+            setState(() {
+              priceValue = double.parse(value);
+            });
+          },
+        ),
+        RaisedButton(
+          child: Text('Save Product'),
+          onPressed: ,
+        )
+      ],
+    ));
   }
 }
