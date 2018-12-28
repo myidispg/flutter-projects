8b3603a6f725124907a61f8dd915d3619cdb4fcc
myidispg <myidispg@gmail.com>
2018-12-28 20:20:32 +0530

    Added swipe to dismiss functionality

--- easy_list/lib/pages/product_list.dart
+++ easy_list/lib/pages/product_list.dart
@@ -4,33 +4,50 @@ import './product_edit.dart';
 
 class ProductListPage extends StatelessWidget {
   final Function updateProduct;
+  final Function deleteProduct;
   final List<Map<String, dynamic>> products;
 
-  ProductListPage(this.products, this.updateProduct);
+  ProductListPage(this.products, this.updateProduct, this.deleteProduct);
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemBuilder: (BuildContext context, int index) {
-        return ListTile(
-          leading: Image.asset(products[index]['image']),
-          title: Text(products[index]['title']),
-          trailing: IconButton(
-            icon: Icon(Icons.edit),
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (BuildContext context) {
-                    return ProductEditPage(
-                      product: products[index],
-                      updateProduct: updateProduct,
-                      productIndex: index,
-                    );
-                  },
-                ),
-              );
-            },
+        return Dismissible(
+          key: Key(products[index]['title']),
+          background: Container(
+            color: Colors.red,
           ),
+          onDismissed: (DismissDirection direction){
+            if(direction == DismissDirection.endToStart){
+              deleteProduct(index);
+            }
+        },// The key must be unique to identify the list title.
+          child: Column(children: <Widget>[
+            ListTile(
+              leading: CircleAvatar(
+                  backgroundImage: AssetImage(products[index]['image'])),
+              title: Text(products[index]['title']),
+              subtitle: Text('\$${products[index]['price'].toString()}'),
+              trailing: IconButton(
+                icon: Icon(Icons.edit),
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (BuildContext context) {
+                        return ProductEditPage(
+                          product: products[index],
+                          updateProduct: updateProduct,
+                          productIndex: index,
+                        );
+                      },
+                    ),
+                  );
+                },
+              ),
+            ),
+            Divider()
+          ]),
         );
       },
       itemCount: products.length,
